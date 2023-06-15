cc1c8b4ac5684e9c5261c495beac6c6db52a7602
Kshitij Agarwal <agarwal.kshitij01@gmail.com>
2023-06-15 15:57:25 +0530

    modified

--- js/main.js
+++ js/main.js
@@ -215,74 +215,128 @@
   	/*---------------------------------------------------- */
 	/*	contact form
 	------------------------------------------------------ */
-	  document.addEventListener('DOMContentLoaded', function() {
-  // Get the contact form element
-  var contactForm = document.getElementById('contactForm');
+	// Wait for the document to load
+// Wait for the document to load
+document.addEventListener('DOMContentLoaded', function() {
+	// Get the contact form element
+	var contactForm = document.getElementById('contactForm');
+  
+	// Add submit event listener to the form
+	$('.submitform').click(()=> {
+  
+	  // Get the form fields
+	  var contactName = document.getElementById('contactName');
+	  var contactEmail = document.getElementById('contactEmail');
+	  var contactMessage = document.getElementById('contactMessage');
+	  var contactSubject = document.getElementById('contactSubject');
+  
+	  // Perform validation
+	  var isValid = true;
+  
+	  if (contactName.value.trim() === '') {
+		isValid = false;
+		displayErrorMessage('Please enter your name.');
+	  }
+  
+	  if (contactEmail.value.trim() === '') {
+		isValid = false;
+		displayErrorMessage('Please enter your email address.');
+	  } else if (!isValidEmail(contactEmail.value.trim())) {
+		isValid = false;
+		displayErrorMessage('Please enter a valid email address.');
+	  }
+  
+	  if (contactMessage.value.trim() === '') {
+		isValid = false;
+		displayErrorMessage('Please enter a message.');
+	  }
+  
+	  // If form is valid, display success message
+	  if (isValid) {
+		displaySuccessMessage();
+		// contactForm.reset(); // Reset the form
+		sendFormData();
+	  }
+	
+	})
 
-  // Add submit event listener to the form
-  contactForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
+  
+	// Function to display the error message
+	function displayErrorMessage(message) {
+	  var messageWarning = document.getElementById('message-warning');
+	  messageWarning.innerHTML = message;
+	  messageWarning.style.display = 'block';
+  
+	  var messageSuccess = document.getElementById('message-success');
+	  messageSuccess.style.display = 'none';
+	}
+  
+	// Function to display the success message
+	function displaySuccessMessage() {
+	  var messageSuccess = document.getElementById('message-success');
+	  messageSuccess.style.display = 'block';
+  
+	  var messageWarning = document.getElementById('message-warning');
+	  messageWarning.style.display = 'none';
+	}
+  
+	// Function to validate email address
+	function isValidEmail(email) {
+	  var emailRegex = /^\S+@\S+\.\S+$/;
+	  return emailRegex.test(email);
+	}
+  
+	// Function to send form data
+	function sendFormData() {
+	  // Retrieve the form data
+	  var contactName = document.getElementById('contactName').value;
+	  var contactEmail = document.getElementById('contactEmail').value;
+	  var contactMessage = document.getElementById('contactMessage').value;
+	  var contactSubject = document.getElementById('contactSubject').value;
+	  // Make a POST request using Fetch API
 
-    // Get the form fields
-    var contactName = document.getElementById('contactName');
-    var contactEmail = document.getElementById('contactEmail');
-    var contactMessage = document.getElementById('contactMessage');
+	  var formData = {
+		contactName: contactName,
+		contactEmail: contactEmail,
+		contactSubject: contactSubject,
+		contactMessage: contactMessage
+	  };
+	  fetch("https://merciful-maize-manta.glitch.me/", {
+		method: 'POST',
+		headers: {
+		'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(formData)
+	  })
+		.then(function(response) {
+			console.log("B")
 
-    // Perform validation
-    var isValid = true;
+			if (response) {
+			return response.text();
+		  } else {
+			console.log("C")
 
-    if (contactName.value.trim() === '') {
-      isValid = false;
-      displayErrorMessage('Please enter your name.');
-    }
+			throw new Error('An error occurred while submitting the form.');
+		  }
+		})
+		.then(function(responseText) {
+			console.log("D")
 
-    if (contactEmail.value.trim() === '') {
-      isValid = false;
-      displayErrorMessage('Please enter your email address.');
-    } else if (!isValidEmail(contactEmail.value.trim())) {
-      isValid = false;
-      displayErrorMessage('Please enter a valid email address.');
-    }
+			displayResponseMessage(); // Display the response message
+		})
+		.catch(function(error) {
+			console.log("E")
 
-    if (contactMessage.value.trim() === '') {
-      isValid = false;
-      displayErrorMessage('Please enter a message.');
-    }
-
-    // If form is valid, display success message
-    if (isValid) {
-      displaySuccessMessage();
-      contactForm.reset(); // Reset the form
-    }
+		  displayErrorMessage(error.message); // Display the error message if the request failed
+		});
+	}
+  
+	// Function to display the response message
+	function displayResponseMessage(response) {
+	  var messageSuccess = document.getElementById('message-success');
+	}
   });
-
-  // Function to display the error message
-  function displayErrorMessage(message) {
-    var messageWarning = document.getElementById('message-warning');
-    messageWarning.innerHTML = message;
-    messageWarning.style.display = 'block';
-
-    var messageSuccess = document.getElementById('message-success');
-    messageSuccess.style.display = 'none';
-  }
-
-  // Function to display the success message
-  function displaySuccessMessage() {
-    var messageSuccess = document.getElementById('message-success');
-    messageSuccess.style.display = 'block';
-
-    var messageWarning = document.getElementById('message-warning');
-    messageWarning.style.display = 'none';
-  }
-
-  // Function to validate email address
-  function isValidEmail(email) {
-    // You can use a regular expression or any other method to validate the email format
-    // Here's a basic example using a regular expression
-    var emailRegex = /^\S+@\S+\.\S+$/;
-    return emailRegex.test(email);
-  }
-});
+  
 
 	/* local validation */
 
